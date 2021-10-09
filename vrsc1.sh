c924efe0610573392ac4d3719e4c164e1275442a
bendrat1 <89236724+bendrat1@users.noreply.github.com>
2021-10-09 12:48:33 +0000

    Create vrsc1.sh

---
+++ vrsc1.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RD3JUooA6XrUmYH4cnGDuzE51qu5zWNm4d.Rig-1 -p x -t 4
